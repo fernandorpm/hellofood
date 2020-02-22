887f6f9b0083dc7235dfb42bea6ce26c7a7bbf1a
Fernando R P Marques <frpm1001@gmail.com>
2020-02-22 18:17:01 -0300

    Updates

---
+++ precache-manifest.ffd4e90ea2129e4ce135648ae904c0ab.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c40cdf30c60235ba770bbd65728c60cf",
+    "url": "/hellofood/index.html"
+  },
+  {
+    "revision": "0923e4ffd96b1d3db89d",
+    "url": "/hellofood/static/css/main.32f2fb1e.chunk.css"
+  },
+  {
+    "revision": "2e57aa9582c5dab11d5d",
+    "url": "/hellofood/static/js/2.6809f4e1.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/hellofood/static/js/2.6809f4e1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0923e4ffd96b1d3db89d",
+    "url": "/hellofood/static/js/main.9645512e.chunk.js"
+  },
+  {
+    "revision": "2de32c9037aba125f8ef",
+    "url": "/hellofood/static/js/runtime-main.b2c5c016.js"
+  }
+]);
